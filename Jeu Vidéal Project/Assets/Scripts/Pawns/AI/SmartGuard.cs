fd0b130e531de2271f755b528d59ccaacbe882e3
GigaPaul <paul.vincent.web@gmail.com>
2023-03-13 21:20:33 +0100

    Smart Pawn abstract class

---
+++ Jeu Vidéal Project/Assets/Scripts/Pawns/AI/SmartGuard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartGuard : SmartPawn
+{
+    protected override void Routine()
+    {
+        Debug.Log("test");
+    }
+}
---
+++ Jeu Vidéal Project/Assets/Scripts/Pawns/AI/SmartPawn.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Pawn))]
+public abstract class SmartPawn : MonoBehaviour
+{
+    public Pawn _Pawn;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Pawn = GetComponent<Pawn>();
+        InvokeRepeating(nameof(RoutineController), 0, 1);
+    }
+
+
+
+
+
+    private void RoutineController()
+    {
+        // If the pawn is dead
+        if (!_Pawn.IsAlive)
+        {
+            return;
+        }
+
+        // If the pawn is moving
+        if (_Pawn.IsMoving())
+        {
+            return;
+        }
+
+        // If the pawn has tasks to do
+        if (!_Pawn.ActionManager.QueueIsEmpty())
+        {
+            return;
+        }
+
+        // If the spawn is playable
+        if (_Pawn.IsPlayable())
+        {
+            return;
+        }
+
+        // Start AI routine
+        Routine();
+    }
+
+
+
+
+
+    protected abstract void Routine();
+}
